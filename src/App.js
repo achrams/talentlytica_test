e14a918f0e44897d2bdd4d958471d9c181aecb06
Achmad Ramadhan Tamherwarin <ramadhanovic@gmail.com>
2023-06-04 18:26:09 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import { format } from "./penilaian";
 function App() {
+  const angka = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const aspect = [1, 2, 3, 4];
+  const [nilai,setNilai] = useState(format)
+
+  const formPenilaian = (i) => {
+    return aspect.map((el) => (
+      <div className="col-lg-2 pl-0" key={'aspek_' + el}>
+        <select name="nilai" className="w-100" key={'select_' + el} onChange={e => changeNilai(el, i, e.target.value)}>
+          {angka.map((val) => (
+            <option key={'form_mahasiswa_' + val} value={val}>{val}</option>
+          ))}
+        </select>
+      </div>
+    ));
+  };
+
+  const changeNilai = (aspect, index, val) => {
+    console.log('keganti')
+    let x = nilai
+    x['aspek_penilaian_' + aspect]['mahasiswa_' + index] = val
+    setNilai(x)
+  }
+
+  const simpan = () => {
+    console.log(nilai)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="col-lg-12 my-5">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-4"></div>
+            {aspect.map((val) => (
+              <div className="col-lg-2 pl-0" key={'aspek_penilaian_' + val}>Aspek Penilaian {val}</div>
+            ))}
+          </div>
+          {angka.map((i) => (
+            <div className="row justify-content-center my-2 p-2 border rounded" key={'div_mahasiswa_' + i}>
+              <div className="col-lg-4" key={'mahasiswa_' + i}>Mahasiswa {i}</div>
+              {formPenilaian(i)}
+            </div>
+          ))}
+          <div className="row justify-content-end my-4">
+            <div className="col-lg-2">
+              <button className="btn btn-primary w-100" onClick={simpan}>Simpan</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
